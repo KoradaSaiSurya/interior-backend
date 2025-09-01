a002cdc7939a2b66f84728cd22fdd33bed57c875
Korada Sai Surya <saisuryakorada2003@gmail.com>
2025-09-01 23:53:45 +0530

    ready backend

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -33,3 +34,5 @@ mongoose.connect(process.env.MONGO_URI, {
   app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
 })
 .catch((err) => console.error("❌ DB Connection Error:", err));
+
+
